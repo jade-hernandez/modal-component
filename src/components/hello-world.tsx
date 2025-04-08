32a5a03fd6cb5dca8ede455ce305287d5a59aeee
Jade Hernandez <jade.hernandez88@gmail.com>
2025-04-08 12:51:42 +0200

    Add Tooltip component for enhanced UI interactions

--- src/components/hello-world.tsx
+++ src/components/hello-world.tsx
@@ -6,6 +6,7 @@ import { Button } from "@/ui/button";
 import { ToggleSwitch } from "@/ui/toggle-button";
 import { useRef, useState } from "react";
 import { PriceCardExample } from "./price-card-exemple";
+import { Tooltip } from "./ui/tooltip";
 
 export default function HelloWorld() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -20,10 +21,16 @@ export default function HelloWorld() {
   useFocusTrap(modalRef, isModalOpen);
 
   return (
-    <div className='p-4'>
+    <div className=''>
       <div className='p-4'>
         <PriceCardExample />
+        <Tooltip content="This is a tooltip" position="top">
+          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+            Hover me
+          </button>
+        </Tooltip>
       </div>
+
       {/* Trigger button */}
       <Button
         onClick={() => setIsModalOpen(true)}
@@ -87,6 +94,7 @@ export default function HelloWorld() {
                 variant='secondary'
               />
             </div>
+
           </div>
         </div>
       )}
